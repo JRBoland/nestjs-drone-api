5f419f80f8e5d61e552bec626cd3738a1a3b9982
James Boland <102449868+JRBoland@users.noreply.github.com>
2024-02-21 19:37:51 +1100

    created controllers, services and modules for each model (users, drones, pilots, flights)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PilotsModule } from './pilots/pilots.module';
+import { DronesModule } from './drones/drones.module';
+import { FlightsModule } from './flights/flights.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PilotsModule, DronesModule, FlightsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/pilots/pilots.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PilotsService } from './pilots.service';
+import { PilotsController } from './pilots.controller';
+
+@Module({
+  providers: [PilotsService],
+  controllers: [PilotsController]
+})
+export class PilotsModule {}
