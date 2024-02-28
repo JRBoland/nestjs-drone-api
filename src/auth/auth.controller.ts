fbda3f290ee3f3bed8ff6faefde7b3bbe7e63888
James Boland <102449868+JRBoland@users.noreply.github.com>
2024-02-28 21:59:23 +1100

    auth jwt passport (partial)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,15 +15,15 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
-  }
-
-  @UseGuards(AuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+  //@HttpCode(HttpStatus.OK)
+  //@Post('login')
+  //signIn(@Body() signInDto: Record<string, any>) {
+  //  return this.authService.signIn(signInDto.username, signInDto.//password);
+  //}
+  //
+  //@UseGuards(AuthGuard)
+  //@Get('profile')
+  //getProfile(@Request() req) {
+  //  return req.user;
+  //}
 }
